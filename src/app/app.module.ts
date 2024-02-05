6dd3dfbb2f0959af79781eec1b8fa6810c16d339
ckj9608 <chandankumarjha1111@gmail.com>
2024-02-05 22:33:30 +0530

    Integrated ngx-select-ex and ng2-file-upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxSelectModule } from 'ngx-select-ex';
+import { FileUploadModule } from 'ng2-file-upload';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxSelectModule,
+    FileUploadModule
   ],
   providers: [],
   bootstrap: [AppComponent]
